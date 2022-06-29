d7bdc29c80d73d4abe5dc85ac212d259fd1e7bbc
AntonPlish <antonplish@gmail.com>
2022-06-29 11:44:53 +0300

    Commit

--- src/news-service.js
+++ src/news-service.js
@@ -6,7 +6,7 @@ export default class NewsApiService {
         this.page = 1;
     }
 
-    fetchArticles() {
+    async fetchArticles() {
         return await (`https://pixabay.com/api/?key=28319602-4eb5fc2c807c8422daa970660&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
             .then(response => response.json())
             .then(data => {
