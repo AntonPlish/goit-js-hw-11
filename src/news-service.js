768ff2985996015045a99082f89ae713ff384d69
AntonPlish <antonplish@gmail.com>
2022-06-29 11:43:01 +0300

    Commit without axios

--- src/news-service.js
+++ src/news-service.js
@@ -6,9 +6,9 @@ export default class NewsApiService {
         this.page = 1;
     }
 
-    async fetchArticles() {
-        return await axios.get(`https://pixabay.com/api/?key=28319602-4eb5fc2c807c8422daa970660&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
-            .then(response => response.json)
+    fetchArticles() {
+        return await (`https://pixabay.com/api/?key=28319602-4eb5fc2c807c8422daa970660&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
+            .then(response => response.json())
             .then(data => {
                 this.page += 1;
                 return data;
